01183894302f094c1b441d395a38094224cbffe7
AlkinBayrak <alkinbayrak01@gmail.com>
2024-10-25 12:52:58 +0300

    BookStore CityPage Add Page Init

---
+++ BookStore.WebApp/BookStore.WebApp/Models/AuthorViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BookStore.WebApp.Models
+{
+    public class AuthorViewModel
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateOnly BirthDate { get; set; }
+        public string Cityname { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ BookStore.WebApp/BookStore.WebApp/Models/PublisherViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BookStore.WebApp.Models
+{
+    public class PublisherViewModel
+    {
+        public string Name { get; set; }
+
+        public string Adress { get; set; }
+
+        public string City { get; set; }
+
+        public bool IsActive { get; set; }
+
+
+    }
+}
