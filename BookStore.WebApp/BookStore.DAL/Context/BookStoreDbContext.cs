b0d61aa58d18b2f9fea31e6d9fb45659db3fa124
AlkinBayrak <alkinbayrak01@gmail.com>
2024-10-25 14:01:51 +0300

    BookStore Db connection First Init

---
+++ BookStore.WebApp/BookStore.DAL/Context/BookStoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Context
+{
+    public class BookStoreDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=VMI1229542;Database=OkulDb2;Trusted_Connection=true;Trustservercertificate=true");
+        }
+    }
+}
