58cb11a4671abed7a969eee28e521c82b858ea60
AlkinBayrak <alkinbayrak01@gmail.com>
2024-10-28 12:21:01 +0300

    DbContext bağlandı,Edit,Update,Remove tuşları yapıldı

---
+++ BookStore.WebApp/BookStore.WebApp/Context/BookStoreDbContext.cs
@@ -0,0 +1,32 @@
+﻿using BookStore.WebApp.Context.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.WebApp.Context
+{
+	public class BookStoreDbContext : DbContext
+	{
+        #region OldCons
+        //private static string _connectionString;
+
+        //      public BookStoreDbContext(IConfiguration configuration)
+        //      {
+        //	_connectionString = configuration.GetConnectionString("BookStoreDbBilkent");
+        //	//var test = configuration.GetSection("ConnectionStrings:BookStoreDbBilkent");
+        //	//Console.WriteLine("ConStr :" + _connectionString);
+        // //         Console.WriteLine("test:" + test);
+
+        //} 
+        #endregion
+        public BookStoreDbContext(DbContextOptions contextOptions) : base(contextOptions)
+        {
+            
+        }
+        public DbSet<City> Cities { get; set; }
+        #region OldConfig
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //	optionsBuilder.UseSqlServer(_connectionString);
+        //} 
+        #endregion
+    }
+}
--- BookStore.WebApp/BookStore.WebApp/Controllers/CityController.cs
+++ BookStore.WebApp/BookStore.WebApp/Controllers/CityController.cs
@@ -1,18 +1,49 @@
-﻿using BookStore.WebApp.Models;
+﻿using AutoMapper;
+using BookStore.WebApp.Context;
+using BookStore.WebApp.Context.Entities.Concrete;
+using BookStore.WebApp.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.WebApp.Controllers
 {
 	public class CityController : Controller
 	{
-		public IActionResult Index()
+        //private readonly IConfiguration _configuration;
+        //public CityController(IConfiguration configuration)
+        //{
+        //    _configuration = configuration;
+        //}
+
+        private readonly BookStoreDbContext _dbContext;
+        private MapperConfiguration _mapCongif;
+		private IMapper _mapper;
+
+        public CityController(BookStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            _mapCongif = new MapperConfiguration(
+                cfg=>cfg.CreateMap<CityViewModel,City>()
+                .ForMember(dest=>dest.IsActive,opt => opt.MapFrom(src=>src.Status))
+                .ReverseMap());
+            _mapper = new Mapper(_mapCongif);
+        }
+
+        public IActionResult Index()
 		{
-			return View();
+			List<City> cities = _dbContext.Cities.Where(c=>c.IsDeleted == false).ToList();
+
+            List<CityViewModel> model = _mapper.Map<List<CityViewModel>>(cities);
+
+			return View(model);
 		}
 
         public IActionResult New()
         {
 			CityViewModel model = new CityViewModel();
+
+           
 
             return View(model);
         }
@@ -21,9 +52,59 @@ namespace BookStore.WebApp.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Add(CityViewModel model)
         {
+			//DbContextOptionsBuilder dbContextOptionBuilder = new DbContextOptionsBuilder<BookStoreDbContext>();
+   //         dbContextOptionBuilder.UseSqlServer("Server=VMI1229542;Database=BookStoreDb;Trusted_Connection=true;Trustservercertificate=true");
 
+   //         DbContextOptions dbContextOptions = dbContextOptionBuilder.Options;
 
-            return null;
+   //         BookStoreDbContext bookStoreDbContext = new BookStoreDbContext(dbContextOptions);
+
+            City city = _mapper.Map<City>(model);
+
+            _dbContext.Cities.Add(city);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int id)
+        {
+           City city = _dbContext.Cities.Where(c => c.Id == id).FirstOrDefault();
+
+            CityViewModel model = _mapper.Map<CityViewModel>(city);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(CityViewModel model)
+        {   
+
+            City city = _mapper.Map<City>(model);
+
+            _dbContext.Cities.Update(city);
+            _dbContext.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+       
+        public IActionResult Remove(int id)
+        {
+            City city = _dbContext.Cities.Where(c => c.Id == id).FirstOrDefault();
+
+            if(city == null)
+            {
+                city.IsDeleted = true;
+                city.Deleted = DateTime.Now;
+
+                _dbContext.Cities.Update(city);
+                _dbContext.SaveChanges();
+            }
+            
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
